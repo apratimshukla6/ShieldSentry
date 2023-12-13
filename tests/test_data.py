8c07d52aa7cff8f3f2945694f085552f118c529a
Apratim Shukla <apratimshukla6@gmail.com>
2023-12-12 16:45:24 -0800

    Update test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,18 +1,22 @@
+import unittest
 from shieldsentry.shieldsentry import ShieldSentry
 
-# Assuming the SecureInputHandler class and specification JSON are properly set up
+class TestShieldSentry(unittest.TestCase):
+    def setUp(self):
+        # This method will be run before each test
+        self.input_handler = ShieldSentry()
 
-# Create an instance of the handler
-input_handler = ShieldSentry()
+    def test_sql_injection_sanitization(self):
+        sql_input = "\" OR \"1\"=\"1"
+        sanitized_sql_input = self.input_handler.sanitize('SQL', sql_input)
+        expected_output = "\\\" OR \\\"1\\\"=\\\"1"  # Expected to escape double quotes and equals
+        self.assertEqual(sanitized_sql_input, expected_output, "SQL sanitization failed")
 
-# SQL Injection Test
-sql_input = "\" OR \"1\"=\"1"
-sanitized_sql_input = input_handler.sanitize('SQL', sql_input)
-print("Original SQLi Input:", sql_input)
-print("Sanitized SQLi Input:", sanitized_sql_input)
+    def test_xss_sanitization(self):
+        xss_input = "<script>alert('XSS')</script>"
+        sanitized_xss_input = self.input_handler.sanitize('HTML', xss_input)
+        expected_output = "&amp;lt;script&amp;gt;alert(&#x27;XSS&#x27;)&amp;lt;&#x2F;script&amp;gt;"  # Expected to escape HTML
+        self.assertEqual(sanitized_xss_input, expected_output, "XSS sanitization failed")
 
-# XSS Test
-xss_input = "<script>alert('XSS')</script>"
-sanitized_xss_input = input_handler.sanitize('HTML', xss_input)
-print("Original XSS Input:", xss_input)
-print("Sanitized XSS Input:", sanitized_xss_input)
+if __name__ == '__main__':
+    unittest.main()
